34c51714667ba8a1bc1d0366ab6c66a0bfdf0a2e
Ben Beehler <ben.beehler@gmail.com>
2022-06-16 18:38:04 -0400

    [Implementation] Add basic tab support

--- src/virgil.c
+++ src/virgil.c
@@ -3,30 +3,40 @@
 #include <stdlib.h>
 #include <webkit2/webkit2.h>
 
+/*
+Rudimentary Virgil window structure
+*/
 struct VirgilWindow {
   GtkWidget *window;
-  GtkWidget *web;
+  GtkNotebook *notebook;
 };
+
+int add_browser_tab(struct VirgilWindow *vwindow) {
+  GtkWidget *web;
+  web = webkit_web_view_new();
+
+  webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web), "https://duckduckgo.com");
+
+  return gtk_notebook_append_page(vwindow->notebook, web, NULL);
+}
 
 /*
 Initialize base window.
 */
 void init(struct VirgilWindow *vwindow) {
   GtkWidget *window;
-  GtkWidget *web;
+  GtkNotebook *notebook;
 
-  web = webkit_web_view_new();
+  notebook = gtk_notebook_new();
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
   gtk_window_set_default_size(GTK_WINDOW(window), 1920, 1080);
   gtk_window_set_title(GTK_WINDOW(window), "Virgil");
 
-  gtk_container_add(GTK_CONTAINER(window), web);
-
-  webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web), "https://duckduckgo.com");
+  gtk_container_add(GTK_CONTAINER(window), notebook);
 
   vwindow->window = window;
-  vwindow->web = web;
+  vwindow->notebook = notebook;
 }
 
 /*
@@ -38,6 +48,9 @@ int main(int argc, char *argv[]) {
   struct VirgilWindow main;
 
   init(&main);
+
+  add_browser_tab(&main);
+  add_browser_tab(&main);
 
   gtk_widget_show_all(main.window);
   gtk_main();
